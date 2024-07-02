6040f0fd76b9b0e16c29a68d6ae28dc4db738cdd
Emanuel <efabio05@icloud.com>
2024-07-01 17:38:10 -0700

    created scene for game

---
+++ game/game.js
@@ -0,0 +1,16 @@
+function main()
+{
+    let game_scene = document.getElementById("game-scene");
+
+    /*makes sure webgl is initalized*/
+    const gl = game_scene.getContext("webgl");
+
+    if(gl === null)
+    {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
